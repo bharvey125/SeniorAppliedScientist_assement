42f07d836589fb40ab17b9456d6b861142096c50
brett Mcbrettface <58987540+blharvey@users.noreply.github.com>
2023-12-09 19:28:38 -0600

    adding modeling pipeline to project
    Adding necessary functionality for the modelling pipeline. includes split function, model estimation and evaluation.

--- busarrival-pred/src/busarrival_pred/pipelines/modeling/nodes.py
+++ busarrival-pred/src/busarrival_pred/pipelines/modeling/nodes.py
@@ -2,3 +2,53 @@
 This is a boilerplate pipeline 'modeling'
 generated using Kedro 0.18.14
 """
+import logging
+from typing import Dict,Tuple
+
+import pandas as pd
+from sklearn.linear_model import LinearRegression
+from sklearn.metrics import r2_score    
+from sklearn.model_selection import train_test_split
+
+from sklearn.preprocessing import OneHotEncoder
+from sklearn.compose import ColumnTransformer
+from sklearn.preprocessing import StandardScaler
+from sklearn.pipeline import Pipeline
+
+
+def split_data(data:pd.DataFrame,parameters:Dict) -> Tuple:
+    x = data[parameters["features"]]
+    y = data[parameters["target"]]
+
+    x_train,x_test,y_train,y_test = train_test_split(
+        x,y,test_size = parameters["test_size"],random_state = parameters["random_state"]
+    )
+    return x_train,x_test,y_train,y_test
+
+
+def train_model(x_train:pd.DataFrame,y_train:pd.Series) -> LinearRegression:
+
+    cat_cols = ['name','hour']
+    num_cols = ['windspeed', 'precip', 'Total', 'lag1', 'lag2']
+    enc = OneHotEncoder(handle_unknown='ignore', sparse_output=False)
+    scaler = StandardScaler()
+    transformer = ColumnTransformer([('ohe', enc, cat_cols),
+                                ('num',scaler,num_cols)])
+
+    model = LinearRegression()
+
+    pipe = Pipeline([('trans',transformer),
+                ('model',model)
+                ])
+
+    pipe.fit(x_train,y_train)
+    return pipe
+
+def evaluate_model(model: LinearRegression,x_test:pd.DataFrame,y_test:pd.DataFrame):
+    y_pred = model.predict(x_test)
+    score = r2_score(y_test,y_pred)
+    logger = logging.getLogger(__name__)
+    logger.info("Model has the coefficient R^2 of %.3f on test data." %score) 
+    x_test['target'] = y_test
+    x_test['pred'] = y_pred
+    return x_test
--- busarrival-pred/src/busarrival_pred/pipelines/modeling/pipeline.py
+++ busarrival-pred/src/busarrival_pred/pipelines/modeling/pipeline.py
@@ -3,8 +3,50 @@ This is a boilerplate pipeline 'modeling'
 generated using Kedro 0.18.14
 """
 
-from kedro.pipeline import Pipeline, pipeline
+from kedro.pipeline import Pipeline, pipeline, node
+from .nodes import split_data, train_model,evaluate_model
 
 
 def create_pipeline(**kwargs) -> Pipeline:
-    return pipeline([])
+    return pipeline(
+        [
+            node(
+                func=split_data,
+                inputs=["L1_route1_dataset","params:model_options"],
+                outputs=["x_train_r1","x_test_r1","y_train_r1","y_test_r1"],
+                name="R1_train_test_split_node"
+            ),
+          
+            node(
+                func=split_data,
+                inputs=["L1_route2_dataset","params:model_options"],
+                outputs=["x_train_r2","x_test_r2","y_train_r2","y_test_r2"],
+                name="R2_train_test_split_node"
+            ),
+               node(
+                func=train_model,
+                inputs=["x_train_r1","y_train_r1"],
+                outputs="regressor1",
+                name="model_r1_node"
+            ),
+           
+            node(
+                func=train_model,
+                inputs=["x_train_r2","y_train_r2"],
+                outputs="regressor2",
+                name="model_r2_node"
+            ),
+               node(
+                func=evaluate_model,
+                inputs=["regressor1",'x_test_r1','y_test_r1'],
+                outputs="model_pred1",
+                name="evaluate_model_route1"   
+                ),
+               node(
+                func=evaluate_model,
+                inputs=["regressor2","x_test_r2","y_test_r2"],
+                outputs="model_pred2",
+                name="evaluate_model_route2"
+               )
+
+        ])
